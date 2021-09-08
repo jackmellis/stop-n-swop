7315b3347b59a3b73aa4480e90da555c3636bc3f
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-09-08 08:00:00 +0100

    fixup! perf: load data in parallel when possible

--- src/ui/pages/Home/NewHome.tsx
+++ src/ui/pages/Home/NewHome.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Screen from 'ui/modules/home/new/Screen';
 import RecentlyAdded from './RecentlyAdded';
 import Popular from './Popular';
 
 export default function NewHome() {
-  return <Screen popular={<Popular />} recentlyAdded={<RecentlyAdded />} />;
+  return (
+    <Screen
+      popular={
+        <Suspense fallback={null}>
+          <Popular />
+        </Suspense>
+      }
+      recentlyAdded={
+        <Suspense fallback={null}>
+          <RecentlyAdded />
+        </Suspense>
+      }
+    />
+  );
 }
